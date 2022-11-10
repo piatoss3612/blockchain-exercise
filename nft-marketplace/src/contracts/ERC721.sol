3e018f661350cec4019281b6773e74d453ecc7ff
Hyohwak Lee <crewe1746@naver.com>
2022-11-10 17:38:30 +0900

    Create ERC721 contract and define mint function

---
+++ nft-marketplace/src/contracts/ERC721.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+/*
+    building out the minting function 
+
+    a. nft to point to an address
+    b. keep track of the token ids
+    c. keep track of token owner addresses to token ids
+    d. keep track of how many tokens an owner address has
+    e. create an event that emits a transfer log - contract address, where it is being minted to, the id
+ */
+contract ERC721 {
+    // Mapping from token id to the owner
+    mapping(uint256 => address) private _tokenOwner;
+
+    // Mapping from owner to the number of owned tokens
+    mapping(address => uint256) private _ownedTokensCount;
+
+    function _exists(uint256 tokenId) internal view returns (bool) {
+        address owner = _tokenOwner[tokenId];
+        return owner != address(0);
+    }
+
+    function mint(address to, uint256 tokenId) internal {
+        // requires that the address is not zero
+        require(to != address(0), "ERC721: minting to the zero address");
+        // requires that the token does not minted yet
+        require(!_exists(tokenId), "ERC721: token already minted");
+
+        _tokenOwner[tokenId] = to;
+        _ownedTokensCount[to]++;
+    }
+}
