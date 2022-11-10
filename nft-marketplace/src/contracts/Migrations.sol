f08a98d00fe4e4b908055da85011252c37cb1bf2
Hyohwak Lee <crewe1746@naver.com>
2022-11-10 14:04:30 +0900

    Modify Migrations.sol

--- nft-marketplace/src/contracts/Migrations.sol
+++ nft-marketplace/src/contracts/Migrations.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.4;
 
@@ -11,7 +11,11 @@ contract Migrations {
     }
 
     modifier restricted() {
-        if (msg.sender == owner) _;
+        require(
+            msg.sender == owner,
+            "This function is restricted to the contract's owner"
+        );
+        _;
     }
 
     function setCompleted(uint256 _completed) public restricted {
