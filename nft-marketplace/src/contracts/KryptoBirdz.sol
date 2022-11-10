e8375f3db85305c3fa0aa1c6771ee4236b7d4981
Hyohwak Lee <crewe1746@naver.com>
2022-11-10 14:28:20 +0900

    Create KryptoBirdz contract

---
+++ nft-marketplace/src/contracts/KryptoBirdz.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract KryptoBirdz {
+    string public name;
+    string public symbol;
+
+    constructor() {
+        name = "KryptoBirds";
+        symbol = "KBIRDZ";
+    }
+}
