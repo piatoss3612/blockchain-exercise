d5f76a1ece09da5a34d795524e4596bc39f58d5f
Hyohwak Lee <crewe1746@naver.com>
2022-11-11 20:53:44 +0900

    Add functions to ERC721Enumerable contract and compile

--- nft-marketplace/src/contracts/ERC721Enumerable.sol
+++ nft-marketplace/src/contracts/ERC721Enumerable.sol
@@ -13,17 +13,41 @@ contract ERC721Enumerable is ERC721 {
 
     mapping(uint256 => uint256) private _ownedTokensIndex;
 
-    function totalSupply() external view returns (uint256) {
+    function totalSupply() public view returns (uint256) {
         return _allTokens.length;
     }
 
     // overide _mint function of ERC721
     function _mint(address to, uint256 tokenId) internal override(ERC721) {
         super._mint(to, tokenId);
-        _addTokensToTotalSupply(tokenId);
+        _addTokenToAllTokensEnumeration(tokenId);
+        _addTokenToOwnerEnumeration(to, tokenId);
     }
 
-    function _addTokensToTotalSupply(uint256 tokenId) private {
+    function _addTokenToOwnerEnumeration(address to, uint256 tokenId) private {
+        _ownedTokens[to].push(tokenId);
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length - 1;
+    }
+
+    function tokenByIndex(uint256 index) public view returns (uint256) {
+        require(index < totalSupply(), "global index is out of bounds");
+        return _allTokens[index];
+    }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        public
+        view
+        returns (uint256)
+    {
+        require(
+            index < balanceOf(owner),
+            "owner's token index is out of bounds"
+        );
+        return _ownedTokens[owner][index];
+    }
+
+    function _addTokenToAllTokensEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
         _allTokens.push(tokenId);
     }
 }
