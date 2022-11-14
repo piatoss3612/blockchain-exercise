e5e53b606d712b278248ac264d3fe2a3e0ec596f
Hyohwak Lee <crewe1746@naver.com>
2022-11-14 14:06:09 +0900

    Add navigation bar and display address on it

--- nft-marketplace/src/components/App.js
+++ nft-marketplace/src/components/App.js
@@ -34,7 +34,19 @@ const App = () => {
 
   return (
     <div>
-      <h1>NFT Marketplace</h1>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <div
+          className="navbar-brand col-sm-3 col-md-3 mr-0"
+          style={{ color: "white" }}
+        >
+          Krypto Birdz NFTs
+        </div>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-white">{account}</small>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
