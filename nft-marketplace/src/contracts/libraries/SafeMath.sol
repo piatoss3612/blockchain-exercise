c5433d7db648ec9d8613e03141a92008e6fc4023
Hyohwak Lee <crewe1746@naver.com>
2022-11-14 19:45:37 +0900

    Create SafeMath library

---
+++ nft-marketplace/src/contracts/libraries/SafeMath.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+library SafeMath {
+    function add(uint256 x, uint256 y) internal pure returns (uint256) {
+        uint256 r = x + y;
+        require(r >= x, "SafeMath: Addition Overflow");
+        return r;
+    }
+
+    function sub(uint256 x, uint256 y) internal pure returns (uint256) {
+        require(y <= x, "SafeMath: Subtraction Overflow");
+        uint256 r = x - y;
+        return r;
+    }
+
+    function mul(uint256 x, uint256 y) internal pure returns (uint256) {
+        // gas optimization
+        if (x == 0) {
+            return 0;
+        }
+        uint256 r = x * y;
+        require(r / x == y, "SafeMath: Multiplication Overflow");
+        return r;
+    }
+
+    function div(uint256 x, uint256 y) internal pure returns (uint256) {
+        require(y > 0, "SafeMath: Division by Zero");
+        return x / y;
+    }
+
+    function mod(uint256 x, uint256 y) internal pure returns (uint256) {
+        require(y != 0, "SafeMath: Modulo by Zero");
+        return x % y;
+    }
+}
